725fac8a649212c3af1e4ae9d71c58a44dacba4e
Celso Ricardo dos Santos Bomfim <lufan20@gmail.com>
2026-08-19 22:19:36 -0300

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    env: process.env.NODE_ENV || 'production',
-    port: parseInt(process.env.PORT || '10000', 10),
+    env: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || '3000', 10),
     apiVersion: process.env.API_VERSION || 'v1',
 
     database: {
